7f28da0c5f6d407cdb2822b911a315ce4aa0e20e
César Rocha <66038783+cesar-nascimento@users.noreply.github.com>
2021-06-18 19:12:37 -0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@ CALL python -m venv .venv
 CALL .venv\Scripts\activate.bat
 CALL python -m pip install --upgrade pip
 CALL pip install -r requirements.txt
-CALL pyinstaller entry-point.py --name your_app --onefile
+CALL pip install pyinstaller
+CALL pyinstaller entry-point.py --name your_app --onefile -y
 PAUSE
